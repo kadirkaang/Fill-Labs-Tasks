f47af071f288861ecdafd1e56370525c42791c95
kkg52 <kadirkaang52@gmail.com>
2024-01-07 22:42:44 +0300

    getAllUsers and addNewUser services added

---
+++ Q4/client/services/userServices.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosResponse } from "axios";
+
+interface User {
+    CreatedAt: string | null;
+    UpdatedAt: string | null;
+    DeletedAt: string | null;
+    ID: number | null;
+    Name: string;
+    Surname: string;
+    Email: string;
+}
+
+const userService = {
+    getAllUsers: async (): Promise<User[]> => {
+        try {
+            const response: AxiosResponse<User[]> = await axios.get('http://127.0.0.1:8080/user');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            throw new Error('Failed to fetch users');
+        }
+    },
+    addNewUser: async (userData: User): Promise<User> => {
+        try {
+            console.log(userData);
+            
+            const response: AxiosResponse<User> = await axios.post('http://127.0.0.1:8080/new_user', userData);
+            return response.data;
+        } catch (error) {
+            console.error('Error adding user:', error);
+            throw new Error('Failed to add user');
+        }
+    },
+};
+export default userService;
