08cc888eae1d5cffc2e06e21ed13f5f5971c167f
kkg52 <kadirkaang52@gmail.com>
2024-01-07 19:45:16 +0300

    db connected

--- Q4/server/server.go
+++ Q4/server/server.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/kkg52/user-management/handlers"
+	"github.com/kkg52/user-management/utils"
+)
 
 func main() {
+	if err := utils.InitDB(); err != nil {
+		panic(err)
+	}
+	defer utils.CloseDB()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	app.Get("/user", handlers.GetAllUser)
+	app.Get("/user/:id", handlers.GetUser)
+	app.Post("/new_user", handlers.CreateUser)
 
 	app.Listen(":8080")
 }
