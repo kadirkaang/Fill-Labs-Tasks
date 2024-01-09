f9c7a6b84d40f01b213b4d19dd587778d76ee7e6
kkg52 <kadirkaang52@gmail.com>
2024-01-09 14:34:32 +0300

    data grid changed to mui/x-data-grid

--- Q4/client/app/page.tsx
+++ Q4/client/app/page.tsx
@@ -1,55 +1,77 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import DataComponent from '@/components/DataGrid'
-import { Button } from '@mui/material'
-import userService from "../services/userServices"
+import * as React from 'react';
+import { useEffect } from 'react';
+import { DataGrid, GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
+import { Button } from '@mui/material';
+import userService, { User } from '@/services/userServices';
 
-interface User {
-  CreatedAt: string;
-  UpdatedAt: string;
-  DeletedAt: string | null;
-  ID: number;
-  Name: string;
-  Surname: string;
-  Email: string;
-}
+export default function ControlledSelectionGrid() {
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 130 },
+        { field: 'firstname', headerName: 'First Name', width: 130 },
+        { field: 'lastname', headerName: 'Last Name', width: 130 },
+        { field: 'email', headerName: 'Email', width: 200, },
+    ];
 
-export default function Home() {
-  const [nodes, setNodes] = useState<User[]>([]);
+    const [rows, setRows] = React.useState<any[]>([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const users = await userService.getAllUsers();
-        setNodes(users);
-      } catch (error: any) {
-        console.error('Error:', error.message);
-      }
+    useEffect(() => {
+        getAllData();
+    }, []);
+
+    function getAllData() {
+        userService.getAllUsers()
+            .then(allUser => {
+                const formattedUsers = handleUserData(allUser);
+                setRows(formattedUsers);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
-    fetchData();
-  }, []);
+    function handleUserData(users: User[]) {
+        return users.map(user => ({
+            id: user.ID,
+            firstname: user.FirstName,
+            lastname: user.LastName,
+            email: user.Email
+        }));
+    }
+    const [rowSelectionModel, setRowSelectionModel] =
+        React.useState<GridRowSelectionModel>([]);
 
-  return (
-    <div className='flex justify-center my-20 mx-32'>
-      <div className='flex flex-col flex-1'>
-        <div className='flex justify-between pb-10'>
-          <h2>
-            USER MANAGEMENT
-          </h2>
-          <div>
-            <Button variant="outlined" href='http://localhost:3000/user'>
-              New
-            </Button>
-            <Button variant="outlined">
-              Update
-            </Button>
-            <Button variant="outlined">
-              Delete
-            </Button>
-          </div>
+    return (
+        <div className='flex justify-center my-20 mx-32'>
+            <div className='flex flex-col flex-1'>
+                <div className='flex justify-between pb-10'>
+                    <h2>
+                        USER MANAGEMENT
+                    </h2>
+                    <div>
+                        <Button variant="contained" href='http://localhost:3000/user'>
+                            New
+                        </Button>
+                        <Button variant="contained" href='#'>
+                            Update
+                        </Button>
+                        <Button variant="contained" href='#'>
+                            Delete
+                        </Button>
+                    </div>
+                </div>
+                <div>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        checkboxSelection
+                        onRowSelectionModelChange={(newRowSelectionModel) => {
+                            console.log(newRowSelectionModel);
+                            setRowSelectionModel(newRowSelectionModel);
+                        }}
+                        rowSelectionModel={rowSelectionModel}
+                    />
+                </div>
+            </div>
         </div>
-        <DataComponent nodes={nodes}/>
-      </div>
-    </div>
-  )
+    );
 }
